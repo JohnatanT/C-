31ea858c38aa3c1f0544576efdca68cb4f5c3c36
Johnatan Tavares <johnatan12014@hotmail.com>
2017-12-13 21:48:06 -0200

    Exercicio 02

---
+++ Exercicio02.cs
@@ -0,0 +1,10 @@
+using System;
+public class Exercicio02{
+	public static void Main(){
+		string nome = "Johnatan";
+		string cargo = "Web Developer";
+		float salario = 700;
+
+		Console.Write("Nome: " +nome+ " cargo: " +cargo+ " Salario: " +salario);
+	}
+}
