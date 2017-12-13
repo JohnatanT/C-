635c0d5b458ef5d0ebc52ae10ef775e6a7c3aa08
Johnatan Tavares <johnatan12014@hotmail.com>
2017-12-13 21:43:04 -0200

    Exercicio01

---
+++ Exercicio01.cs
@@ -0,0 +1,12 @@
+using System;
+public class Exercicio01{
+	public static void Main(){
+		int idade = 17;
+		int idadeSandy = 16;
+		int idadeJehnifer = 17;
+
+		float media = (idade+idadeSandy+idadeJehnifer) /3; 
+
+		Console.Write(media)
+	}
+}
