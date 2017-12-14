41f8023cdafe93eeab531370565bd8dee487adfe
Johnatan Tavares <johnatan12014@hotmail.com>
2017-12-13 22:07:49 -0200

    Exercicio 03

---
+++ Exercicio03.cs
@@ -0,0 +1,15 @@
+using System;
+public class Exercicio03{
+	public static void Main(){
+		int idade = 17;
+		bool brasileira = true;
+
+		if(idade >= 18 && brasileira == true){
+			Console.Write("Pode Votar!");
+		}else{
+			Console.Write("Não pode Votar!");
+		}
+
+
+	}
+}
