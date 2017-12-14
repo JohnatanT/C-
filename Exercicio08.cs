9743a54abd1d952f043e48b4def267c63eff5120
Johnatan Tavares <johnatan12014@hotmail.com>
2017-12-14 20:34:58 -0200

    Do While

---
+++ Exercicio08.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int idad = 10;
+            do
+            {
+                Console.WriteLine("Menor de Idade");
+            } while (idad < 9);
+
+            Console.ReadLine();//Deixa o Console Aberto
+        }
+    }
+}
